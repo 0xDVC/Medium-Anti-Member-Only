c4aa43436245140447636709f61a9f3d03676bce
dvc-77 <neilohene@gmail.com>
2024-07-06 00:45:57 +0000

    feat: popup.js to handle events for dialog box

--- chromium/background.js
+++ chromium/background.js
@@ -1,12 +1,23 @@
+function handleActionClick(tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.browserAction.setPopup({ popup: 'popup.html' });
 
-chrome.action.onClicked.addListener((tab) => {
-  // get the current active tab and then redirect the link to freedium
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    var link = activeTab.url;
-    
+    const activeTab = tabs[0];
+    const link = activeTab.url;
+
     chrome.tabs.create({
       url: `https://freedium.cfd/${link}`
-    })
+    });
   });
-})
+}
+
+function handleMessage(request) {
+    if (request.action === 'toggleExtension') {
+      const { url, isEnabled } = request;
+      localStorage.setItem(`enabled-${url}`, isEnabled);
+      console.log(`Extension is ${isEnabled ? 'enabled' : 'disabled'} for ${url}`);
+    }
+}
+
+chrome.action.onClicked.addListener(handleActionClick);
+chrome.runtime.onMessage.addListener(handleMessage);
